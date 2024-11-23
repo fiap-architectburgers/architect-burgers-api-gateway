9c031bfd4c3d84b119bd66f665e73a6e3cc0d7e8
Rodrigo Gomes <gomesrodrigo0481@gmail.com>
2024-11-23 16:06:23 -0300

    Change app integration to route for the 3 microservices

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,8 +13,18 @@ variable "cognito_user_pool_client_secret" {
   type = string
 }
 
-# export TF_VAR_load_balancer_listener_arn="....."
-variable "load_balancer_listener_arn" {
+# export TF_VAR_load_balancer_pagamento_arn="....."
+variable "load_balancer_pagamento_arn" {
   type = string
-  description = "ARN of the load balancer listener (8090) created for the EKS cluster"
+  description = "ARN of the load balancer listener (8090) created for the EKS cluster - Serviço de Pagamentos"
+}
+
+variable "load_balancer_pedidos_arn" {
+  type = string
+  description = "ARN of the load balancer listener (8091) created for the EKS cluster - Serviço de Pedidos"
+}
+
+variable "load_balancer_catalogo_arn" {
+  type = string
+  description = "ARN of the load balancer listener (8092) created for the EKS cluster - Serviço de Catalogo"
 }
